278fffff43965cfe04128a4215886785465f00e9
Ian Page Hands <iphands@gmail.com>
2019-05-17 12:31:50 -0400

    remove dev specific sh files, add local to ignore

--- config/development.aprice.sh
+++
@@ -1,4 +0,0 @@
-export ENT_KEY=/home/aprice/Documents/certs/aprice-key.key
-export ENT_CERT=/home/aprice/Documents/certs/aprice-sans-key.crt
-export ENT_PORT=3000
-export ENT_SUBSHOST=https://subscription.api.redhat.com
