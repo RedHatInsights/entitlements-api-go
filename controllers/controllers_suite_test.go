0a0bfaa5b2322205379f7aa6b36ef8c6d1d514f0
Ian Page Hands <iphands@gmail.com>
2019-05-17 22:06:02 -0400

    in tests init logger right away

--- controllers/controllers_suite_test.go
+++ controllers/controllers_suite_test.go
@@ -3,11 +3,13 @@ package controllers_test
 import (
 	"testing"
 
+	. "github.com/RedHatInsights/entitlements-api-go/logger"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 func TestControllers(t *testing.T) {
+	InitLogger()
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Controllers Suite")
 }
