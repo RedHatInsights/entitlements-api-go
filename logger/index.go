10a901fd4b4e15dabe628beba09292858c280c64
Ian Page Hands <iphands@gmail.com>
2019-05-17 21:37:12 -0400

    disable logging in test

--- logger/index.go
+++ logger/index.go
@@ -1,6 +1,7 @@
 package logger
 
 import (
+	"flag"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
@@ -10,22 +11,33 @@ var logWithStack *zap.Logger
 
 func Logger() *zap.Logger {
 	if (log == nil) {
-		logger, _ := zap.NewProduction(zap.AddStacktrace(zapcore.PanicLevel))
+		logLevel := zapcore.InfoLevel
+		if flag.Lookup("test.v") != nil { logLevel = zapcore.FatalLevel }
+
+		cfg := zapcore.EncoderConfig{
+			TimeKey:        "ts",
+			LevelKey:       "level",
+			NameKey:        "logger",
+			CallerKey:      "caller",
+			MessageKey:     "msg",
+			LineEnding:     zapcore.DefaultLineEnding,
+			EncodeLevel:    zapcore.LowercaseLevelEncoder,
+			EncodeTime:     zapcore.EpochTimeEncoder,
+			EncodeDuration: zapcore.SecondsDurationEncoder,
+			EncodeCaller:   zapcore.ShortCallerEncoder,
+		}
+
+		logger, _ := zap.Config{
+			Encoding:    "json",
+			Level:       zap.NewAtomicLevelAt(logLevel),
+			OutputPaths: []string{"stdout"},
+			ErrorOutputPaths: []string{"stderr"},
+			EncoderConfig: cfg,
+		}.Build()
 
 		defer logger.Sync()
 		log = logger
 	}
 
 	return log
-}
-
-func LoggerWithStack() *zap.Logger {
-	if (logWithStack == nil) {
-		logger, _ := zap.NewProduction()
-
-		defer logger.Sync()
-		logWithStack = logger
-	}
-
-	return logWithStack
 }
