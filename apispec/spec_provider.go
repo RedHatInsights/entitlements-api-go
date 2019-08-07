318c84839a723c0bdc901fe159c2e74077caba59
tahmidefaz <tefaz@redhat.com>
2019-08-07 16:17:43 -0400

    logs for opening file

--- apispec/spec_provider.go
+++ apispec/spec_provider.go
@@ -1,7 +1,9 @@
 package apispec
 
 import (
+	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 
@@ -10,11 +12,15 @@ import (
 
 // OpenAPISpec responds back with the openapi spec
 func OpenAPISpec(r chi.Router) {
-	specFile, err := os.Open("./apispec/api.spec.json")
+	currDir, err := os.Getwd()
+	specDir := string(currDir) + "/apispec/Aapi.spec.json"
+	fmt.Println(specDir)
+	specFile, err := os.Open(specDir)
 
 	if err != nil {
 		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 			w.Write([]byte("OpenApi Spec not available\n"))
+			log.Panic(err)
 		})
 		return
 	}
