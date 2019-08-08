8350674e95946fa276a5b996a2b85cc896eebd57
tahmidefaz <tefaz@redhat.com>
2019-08-08 13:58:59 -0400

    trying with environment variable with viper

--- apispec/spec_provider.go
+++ apispec/spec_provider.go
@@ -6,8 +6,8 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/RedHatInsights/entitlements-api-go/config"
 	"github.com/go-chi/chi"
-	"github.com/spf13/viper"
 )
 
 // OpenAPISpec responds back with the openapi spec
@@ -15,14 +15,14 @@ func OpenAPISpec(r chi.Router) {
 	// currDir, err := os.Getwd()
 	// currDir, _ := filepath.Abs(filepath.Dir(os.Args[0]))
 	// fmt.Println(currDir)
-	var specDir string
-	var options = viper.New()
-	options.SetDefault(specDir, "./apispec/api.spec.json")
-	options.SetEnvPrefix("ENT")
-	options.AutomaticEnv()
-	specFile, err := ioutil.ReadFile(options.GetString(specDir))
-	fmt.Println(options.GetString(specDir))
-
+	// var specDir string
+	// var options = viper.New()
+	// options.SetDefault(specDir, "./apispec/api.spec.json")
+	// options.SetEnvPrefix("ENT")
+	// options.AutomaticEnv()
+	pathpath := config.GetConfig().Options.GetString(config.OpenApiSpecFilePath)
+	fmt.Println(pathpath)
+	specFile, err := ioutil.ReadFile(pathpath)
 	if err != nil {
 		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 			w.Write([]byte("OpenApi Spec not available\n"))
