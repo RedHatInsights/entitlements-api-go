7ef13852c720196763762a80c07e8b63b9f712a8
Tahmid Efaz <tefaz@redhat.com>
2019-10-17 13:19:17 -0400

    Improve Error Handling when bundles.yml fails (#30)
    * throwing fatal when err in YAML using normal log, custom logger throws seg fault
    
    * correctly throwing fatal err when bundles.yml fails
    
    * incorporating the code refactoring that Shaun suggested (#31)

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"github.com/RedHatInsights/entitlements-api-go/config"
+	"github.com/RedHatInsights/entitlements-api-go/controllers"
 	"github.com/RedHatInsights/entitlements-api-go/logger"
 	"github.com/RedHatInsights/entitlements-api-go/server"
+
+	"go.uber.org/zap"
 )
 
 func main() {
 	// Init the logger first thing
 	logger.InitLogger()
+	// init config here
+	if err := controllers.SetBundleInfo(config.GetConfig().Options.GetString(config.Keys.BundleInfoYaml)); err != nil {
+		logger.Log.Fatal("Error reading bundles.yml", zap.Error(err))
+	}
+
 	server.Launch()
 }
