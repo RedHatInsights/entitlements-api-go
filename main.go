cab7d5008ae5e88cc264ee78f6ccd7c2e59e0fd9
Vinicius Belchior <vbelchio@redhat.com>
2021-04-28 15:05:40 -0300

    RHCLOUD-13100: Enabling Sentry (#59)
    * RHCLOUD-13100: Enabling Sentry
    
    * Update main.go
    
    Co-authored-by: Keith Walsh <walsh1kt@gmail.com>
    
    * Update main.go
    
    Co-authored-by: Keith Walsh <walsh1kt@gmail.com>
    
    * Update main.go
    
    Co-authored-by: Keith Walsh <walsh1kt@gmail.com>
    
    * RHCLOUD-13100: Enabling Sentry
    
    * RHCLOUD-13100: Enabling Sentry
    
    * RHCLOUD-13100: Sentry success message
    
    * RHCLOUD-13100: Sentry success message
    
    * Update main.go
    
    Co-authored-by: Keith Walsh <walsh1kt@gmail.com>
    
    Co-authored-by: Keith Walsh <walsh1kt@gmail.com>

--- main.go
+++ main.go
@@ -1,21 +1,45 @@
 package main
 
 import (
+	"os"
+	"time"
+
 	"github.com/RedHatInsights/entitlements-api-go/config"
 	"github.com/RedHatInsights/entitlements-api-go/controllers"
 	"github.com/RedHatInsights/entitlements-api-go/logger"
 	"github.com/RedHatInsights/entitlements-api-go/server"
 
 	"github.com/sirupsen/logrus"
+
+	"github.com/getsentry/sentry-go"
 )
 
 func main() {
+
 	// Init the logger first thing
 	logger.InitLogger()
+
+	var dsn string = os.Getenv("SENTRY_DSN")
+
+	if dsn != "" {
+		err := sentry.Init(sentry.ClientOptions{})
+		if err != nil {
+			logger.Log.WithFields(logrus.Fields{"error": err}).Error("Error loading Sentry SDK")
+		} else {
+			logger.Log.Info("Sentry SDK initialization was successful!")
+		}
+	} else {
+		logger.Log.Info("SENTRY_DSN was not set, skipping Sentry initialization.")
+	}
+
 	// init config here
 	if err := controllers.SetBundleInfo(config.GetConfig().Options.GetString(config.Keys.BundleInfoYaml)); err != nil {
 		logger.Log.WithFields(logrus.Fields{"error": err}).Fatal("Error reading bundles.yml")
 	}
 
 	server.Launch()
+
+	// Flush buffered events before the program terminates.
+	// Set the timeout to the maximum duration the program can afford to wait.
+	defer sentry.Flush(2 * time.Second)
 }
