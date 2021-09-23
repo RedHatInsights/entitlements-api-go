e2803329261aeed9e2bed530298942925a458a4f
casey-williams-rh <76960530+casey-williams-rh@users.noreply.github.com>
2021-09-23 16:16:38 -0400

    Adding NOP logic to new pr_check.sh as a place holder (#66)
    * Adding NOP logic to new pr_check.sh as a place holder
    
    * Change output file name for the PR Checker to junit-dummy.xml

--- pr_check.sh
+++ pr_check.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+export APP_NAME="entitlements"  # name of app-sre "application" folder this component lives in
+export COMPONENT_NAME="entitlements-api-go"  # name of app-sre "resourceTemplate" in deploy.yaml for this component
+export IMAGE="quay.io/cloudservices/entitlements-api-go"  # the image location on quay
+
+# Install bonfire repo/initialize
+CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
+curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
+
+# Build the image and push to quay
+source $CICD_ROOT/build.sh
+
+# NOP action until PR Check logic is added in the future
+mkdir -p $WORKSPACE/artifacts
+cat << EOF > $WORKSPACE/artifacts/junit-dummy.xml
+<testsuite tests="1">
+    <testcase classname="dummy" name="dummytest"/>
+</testsuite>
+EOF
