3a8df391f09fcdcd0c182f25f3a1adb2acf202a5
Daniel Agbay <dagbay@redhat.com>
2022-06-29 17:47:05 -0500

    added metrics to compliance endpoint

--- controllers/compliance.go
+++ controllers/compliance.go
@@ -7,18 +7,35 @@ import (
 	l "github.com/RedHatInsights/entitlements-api-go/logger"
 	"github.com/RedHatInsights/entitlements-api-go/types"
 	"github.com/getsentry/sentry-go"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/sirupsen/logrus"
 	"io"
 	"io/ioutil"
 	"net/http"
+	"strconv"
+	"time"
 )
 
 var screeningPathV1 = "/v1/screening"
 var complianceServiceName = "Export Compliance Service"
 
+var complianceFailure = promauto.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "it_export_compliance_service_failure",
+		Help: "Total number of IT export compliance service failures",
+	},
+	[]string{"code"},
+)
+var complianceTimeHistogram = promauto.NewHistogram(prometheus.HistogramOpts{
+	Name:    "it_export_compliance_service_time_taken",
+	Help:    "Export compliance service latency distributions.",
+	Buckets: prometheus.LinearBuckets(0.25, 0.25, 20),
+})
+
 func Compliance() func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, req *http.Request) {
-		// TODO: time metrics
+		start := time.Now()
 		reqBody, err := ioutil.ReadAll(req.Body)
 		if err != nil {
 			failOnBadRequest(w, "Failed to read request body", err)
@@ -44,6 +61,10 @@ func Compliance() func(http.ResponseWriter, *http.Request) {
 			return
 		}
 
+		complianceTimeTaken := time.Since(start).Seconds()
+		l.Log.WithFields(logrus.Fields{"compliance_call_duration": complianceTimeTaken}).Info("compliance call complete")
+		complianceTimeHistogram.Observe(complianceTimeTaken)
+
 		defer resp.Body.Close()
 		respBody, err := io.ReadAll(resp.Body)
 
@@ -55,6 +76,7 @@ func Compliance() func(http.ResponseWriter, *http.Request) {
 func failOnBadRequest(w http.ResponseWriter, errMsg string, err error) {
 	sentry.CaptureException(err)
 	l.Log.WithFields(logrus.Fields{"error": err}).Error(errMsg)
+	complianceFailure.WithLabelValues(strconv.Itoa(http.StatusBadRequest)).Inc()
 
 	response := types.RequestErrorResponse{
 		Error: types.RequestErrorDetails{
@@ -70,6 +92,8 @@ func failOnBadRequest(w http.ResponseWriter, errMsg string, err error) {
 func failOnComplianceError(w http.ResponseWriter, errMsg string, err error, url string) {
 	sentry.CaptureException(err)
 	l.Log.WithFields(logrus.Fields{"error": err}).Error(errMsg)
+	complianceFailure.WithLabelValues(strconv.Itoa(http.StatusInternalServerError)).Inc()
+
 	response := types.DependencyErrorResponse{
 		Error: types.DependencyErrorDetails{
 			DependencyFailure: true,
