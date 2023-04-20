a2d62ecfff88aebe53e6b194e8c09390215bdf79
casey-williams-rh <caswilli@redhat.com>
2023-04-20 14:22:16 -0400

    Update build_deploy.sh

--- build_deploy.sh
+++ build_deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-env
-
 set -exv
 
 IMAGE="quay.io/cloudservices/entitlements-api-go"
@@ -25,7 +23,15 @@ docker --config="$DOCKER_CONF" login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
 docker --config="$DOCKER_CONF" login -u="$RH_REGISTRY_USER" -p="$RH_REGISTRY_TOKEN" registry.redhat.io
 docker --config="$DOCKER_CONF" build --no-cache -t "${IMAGE}:${IMAGE_TAG}" .
 docker --config="$DOCKER_CONF" push "${IMAGE}:${IMAGE_TAG}"
-docker --config="$DOCKER_CONF" tag "${IMAGE}:${IMAGE_TAG}" "${IMAGE}:${LATEST_TAG}"
-docker --config="$DOCKER_CONF" push "${IMAGE}:${LATEST_TAG}"
-docker --config="$DOCKER_CONF" tag "${IMAGE}:${IMAGE_TAG}" "${IMAGE}:${QA_TAG}"
-docker --config="$DOCKER_CONF" push "${IMAGE}:${QA_TAG}"
+
+if [[ $GIT_BRANCH != *"security-compliance"* ]]; then
+    docker --config="$DOCKER_CONF" tag "${IMAGE}:${IMAGE_TAG}" "${IMAGE}:${LATEST_TAG}"
+    docker --config="$DOCKER_CONF" push "${IMAGE}:${LATEST_TAG}"
+    docker --config="$DOCKER_CONF" tag "${IMAGE}:${IMAGE_TAG}" "${IMAGE}:${QA_TAG}"
+    docker --config="$DOCKER_CONF" push "${IMAGE}:${QA_TAG}"
+fi
+
+if [[ $GIT_BRANCH == *"security-compliance"* ]]; then
+    docker --config="$DOCKER_CONF" tag "${IMAGE}:${IMAGE_TAG}" "${IMAGE}:security-compliance"
+    docker --config="$DOCKER_CONF" push "${IMAGE}:security-compliance"
+fi
