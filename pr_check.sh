9c8ebdf3d046b8d913f3f91b1b364d87f2fb2bc9
Daniel Agbay <dagbay@redhat.com>
2023-08-22 17:12:32 -0500

    update pr check to run unit tests in jenkins builds

--- pr_check.sh
+++ pr_check.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# --------------------------------------------
+# Options that must be configured by app owner
+# --------------------------------------------
 export APP_NAME="entitlements"  # name of app-sre "application" folder this component lives in
 export COMPONENT_NAME="entitlements-api-go"  # name of app-sre "resourceTemplate" in deploy.yaml for this component
 export IMAGE="quay.io/cloudservices/entitlements-api-go"  # the image location on quay
@@ -10,11 +13,5 @@ curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
 
 # Build the image and push to quay
 source $CICD_ROOT/build.sh
-
-# NOP action until PR Check logic is added in the future
-mkdir -p $WORKSPACE/artifacts
-cat << EOF > $WORKSPACE/artifacts/junit-dummy.xml
-<testsuite tests="1">
-    <testcase classname="dummy" name="dummytest"/>
-</testsuite>
-EOF
+make test-all
+source $CICD_ROOT/post_test_results.sh
