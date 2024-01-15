7b62e346eaed113a58c7043ed24b3c84a529451d
Daniel Agbay <Daniel Agbay>
2024-01-15 15:28:06 -0600

    update go version to 1.21

--- scripts/xrhid.sh
+++ scripts/xrhid.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ID='{ "identity" : { "account_number": "540155", "type": "User", "internal": { "org_id": "11009103" }, "user": { "is_org_admin": true } } }'
+ID='{ "identity" : { "account_number": "540155", "type": "User", "internal": { "org_id": "11009103" }, "user": { "is_org_admin": true, "username": "rh-ee-dagbay" } } }'
 echo $ID | base64 | tr -d '\n'
