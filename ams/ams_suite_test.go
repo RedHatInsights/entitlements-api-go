7241c0ae9e81c934f04dd2be97f2712d1322832f
Daniel Agbay <dagbay@redhat.com>
2024-02-28 13:18:10 -0600

    revert test changes

--- ams/ams_suite_test.go
+++ ams/ams_suite_test.go
@@ -3,7 +3,6 @@ package ams
 import (
 	"testing"
 
-	"github.com/RedHatInsights/entitlements-api-go/config"
 	. "github.com/RedHatInsights/entitlements-api-go/logger"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -12,9 +11,5 @@ import (
 func TestAms(t *testing.T) {
 	InitLogger()
 	RegisterFailHandler(Fail)
-	if config.GetConfig().Options.GetBool(config.Keys.DisableSeatManager) {
-		GinkgoWriter.Println("Seats apis are disabled... skipping ams suite")
-		return
-	}
 	RunSpecs(t, "AMS Suite")
 }
--- bop/client_suite_test.go
+++ bop/client_suite_test.go
@@ -3,7 +3,6 @@ package bop
 import (
 	"testing"
 
-	"github.com/RedHatInsights/entitlements-api-go/config"
 	. "github.com/RedHatInsights/entitlements-api-go/logger"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -12,9 +11,5 @@ import (
 func TestBop(t *testing.T) {
 	InitLogger()
 	RegisterFailHandler(Fail)
-	if config.GetConfig().Options.GetBool(config.Keys.DisableSeatManager) {
-		GinkgoWriter.Println("Seats apis are disabled... skipping bop test suite")
-		return
-	}
 	RunSpecs(t, "BOP Suite")
 }
